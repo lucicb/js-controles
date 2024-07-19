4f4ce985109f3801f9c834e97ad5cc8fbdfe9ed0
lucicb <luciabelensanchez155@gmail.com>
2024-07-19 18:48:57 -0400

    holiwis

---
+++ js-controles/app.js
@@ -0,0 +1,15 @@
+/*
+    DOMContentLoaded sirve para controlar
+    que primero se cargue TODO el documento
+    HTML, por ultimo javascript
+*/
+document
+.addEventListener("DOMContentLoaded", function(){
+    alert("Holis");
+});
+
+
+function hola(nombre, apellido) {
+    console.log(nombre, apellido);
+    // codigo reutilizable
+}
