a8ff3a1e7eb8109a5f1f8092064bf8bc61c46aa1
lucicb <luciabelensanchez155@gmail.com>
2024-07-22 20:33:45 -0400

    holaaa

--- js-controles/app.js
+++ js-controles/app.js
@@ -1,15 +1,18 @@
-/*
-    DOMContentLoaded sirve para controlar
-    que primero se cargue TODO el documento
-    HTML, por ultimo javascript
-*/
-document
-.addEventListener("DOMContentLoaded", function(){
-    alert("Holis");
+document.addEventListener("DOMContentLoaded",function(){
+    //console.log(document.getElementById("txt_nombre").value);
+    //console.log(document.getElementById("txt_apellido").value);
+});
+const btn_mostrar=document.getElementById("btn_mostrar");
+btn_mostrar.addEventListener("click",function(){
+    //alert("hola");
+    const nombres = document.getElementById("txt_nombre").value;
+    const apellido = document.getElementById("txt_apellido").value;
+    const cedula = document.getElementById("txt_cedula").value;
+    const direccion = document.getElementById("txt_direccion").value;
+    console.log('nombres: ' +nombres+ ', apellidos: ' +apellido+ ', cedula: ' + cedula+ ', direccion: ' + direccion);
+    alert('nombres: ' +nombres+ ', apellidos: ' +apellido+ ', cedula: ' + cedula+ ', direccion: ' + direccion);
 });
 
-
-function hola(nombre, apellido) {
-    console.log(nombre, apellido);
-    // codigo reutilizable
-}
+function hol(nombre,apellido){
+    console.log(nombre,apellido);
+};
